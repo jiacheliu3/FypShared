3c7f7d255134caee4eb18fdcaedcbd8bf7484515
jiacheliu3 <jiacheliu3@gmail.com>
2017-12-14 06:37:43 +0000

    Try to deploy

---
+++ grails-app/domain/codebigbrosub/Task.groovy
@@ -0,0 +1,17 @@
+package codebigbrosub
+
+class Task {
+
+	def  preCrawlTime;
+	def keywordTime;
+	def statTime;
+	def interationTime;
+	def clusterTime;
+	def timelineTime;
+	def deepCrawlTime;
+	int taskSize;
+	
+	static constraints = {
+	}
+	
+}
